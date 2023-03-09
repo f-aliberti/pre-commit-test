bfda374e6eae9d91efee587118edf3886cd69e7a
Francesco Oreste Aliberti <f.aliberti@eco-mind.eu>
2023-03-09 18:30:33 +0100

    Adde: hooks for multi-module

---
+++ e4t_post_checkout_multi.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+#FOR POST-CHECKOUT
+
+#This hook is invoked when a git-checkout[1] or git-switch[1] is run after having updated the worktree.
+#The hook is given three parameters: the ref of the previous HEAD,
+#the ref of the new HEAD (which may or may not have changed),
+#and a flag indicating whether the checkout was a branch checkout (changing branches, flag=1)
+#or a file checkout (retrieving a file from the index, flag=0).
+#This hook cannot affect the outcome of git switch or git checkout,
+#other than that the hook’s exit status becomes the exit status of these two commands.
+# documentation: https://git-scm.com/docs/githooks
+
+if [ $PRE_COMMIT_CHECKOUT_TYPE != '1' ]; then
+    echo 'git checkout did not checkout a branch - quitting'
+    exit
+fi
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $branch != feature/* ]]; then
+    echo 'Not Feature branch'
+    exit
+fi
+
+branch="${branch#'feature/'}"
+
+#echo $branch
+[[ $branch =~ ^E4T[0-9]{4}$ ]] || echo 'Rename branch with E4TXXXX (X are [0-9] digit)'
+
+# get current version of the top level pom
+current_version=$(mvn help:evaluate -Dexpression=project.version | grep -v '\[.*')
+
+echo 'current version:' $current_version
+
+current_version_cutted=$(echo $current_version | cut -f1 -d \-)
+
+new_version=$current_version_cutted'-'$branch'-SNAPSHOT'
+
+echo 'version to set:' $new_version
+
+if [[ $current_version != $new_version ]]; then
+    if mvn versions:set-property -Dproperty=revision -DnewVersion=$new_version; then
+        echo "Version changed via Maven"
+    else
+        echo "Version change via Maven FAILED"
+        exit 1
+    fi
+else
+    echo "Nothing to change via maven"
+fi
